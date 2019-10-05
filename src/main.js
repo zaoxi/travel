b595c36e535b485384b7c150eccfa62464e05cc6
zaoxi <904037331@qq.com>
2019-10-05 17:30:28 +0800

    '全局资源准备成功'

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import FastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
+FastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>
+      home
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'home'
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
